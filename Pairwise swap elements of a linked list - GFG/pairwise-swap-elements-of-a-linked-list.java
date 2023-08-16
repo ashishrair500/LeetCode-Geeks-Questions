14f85979dac691c103e977f1868f85046d50a2f4
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-08-16 22:10:17 +0530

    Added solution - LeetHub

---
+++ Pairwise swap elements of a linked list - GFG/pairwise-swap-elements-of-a-linked-list.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        	int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            HashMap<Node, Integer> mp = new HashMap<Node, Integer>();
+            mp.put(head, head.data); 
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+                mp.put(tail, tail.data); 
+            }
+            
+            Node failure = new Node(-1);
+            
+            Solution g = new Solution();
+            head = g.pairwiseSwap(head);
+            
+            Node temp = head;
+            int f = 0;
+            while (temp != null) 
+    		{ 
+    			if(mp.get(temp) != temp.data){
+    			    f = 1;
+    			}
+    			temp = temp.next; 
+    		} 
+            
+            if(f==1){
+                printList(failure); 
+            }
+            else{
+                printList(head); 
+            }
+            t--;
+        }
+    } 
+} 
+   
+
+// } Driver Code Ends
+
+
+/* node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+
+class Solution {
+    // Function to pairwise swap elements of a linked list.
+    // It should returns head of the modified list
+    public Node pairwiseSwap(Node head)
+    {
+        if(head.next==null) return head;
+        Node curr1=head;
+        Node curr2=head.next;
+        Node dummy=new Node(-1);
+        Node prev=dummy;
+    //  Node t=null;
+     //   int c=1;
+        
+        while(curr2!=null &&curr1!=null){
+            
+          curr1.next=curr2.next;
+          
+          curr2.next=curr1;
+          prev.next=curr2;
+          prev=curr1;
+          curr1=curr1.next;
+          if(curr1==null) break;
+          curr2=curr1.next;
+          
+          
+        }
+        
+        return dummy.next;
+        // code here
+    }
+}
